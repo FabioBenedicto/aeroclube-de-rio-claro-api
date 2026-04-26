e63fc310e6122df995383cfff6566a6898e93171
Fábio Henrique Benedicto <fabiobenedicto@pecege.com>
2026-04-26 07:11:51 -0300

    feat: wire all modules in AppModule (planes, customers, payables, invoices, credits, dashboard)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { FlightsModule } from './flights/flights.module';
 import { ReceivablesModule } from './receivables/receivables.module';
+import { PlanesModule } from './planes/planes.module';
+import { CustomersModule } from './customers/customers.module';
+import { PayablesModule } from './payables/payables.module';
+import { InvoicesModule } from './invoices/invoices.module';
+import { CreditsModule } from './credits/credits.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -12,6 +18,12 @@ import { ReceivablesModule } from './receivables/receivables.module';
     AuthModule,
     FlightsModule,
     ReceivablesModule,
+    PlanesModule,
+    CustomersModule,
+    PayablesModule,
+    InvoicesModule,
+    CreditsModule,
+    DashboardModule,
   ],
 })
 export class AppModule {}
